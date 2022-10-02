1d84a95db51298e46655bb681727c560e5135496
Mariam Nasser <MariamNasser45>
2022-10-02 01:28:49 -0700

    Add project files.

---
+++ Crud.Net/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace Crud.Net.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string ? Name { get; set; }
+        public string? History { get; set; }
+        public float Rate { get; set; }
+
+        // define relationship between tables
+        public int? GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+
+    }
+}
